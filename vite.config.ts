f31b552f17ece88d9145f8f2902ed7b6f344e08b
vitamindipak-cyber <vitamindipak@gmail.com>
2026-09-15 15:17:09 +0545

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: process.env.VITE_BASE_PATH || './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
